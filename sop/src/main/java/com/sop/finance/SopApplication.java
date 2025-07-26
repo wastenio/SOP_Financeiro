3fcf6f937fcaba5ab2d7bb5df49ec96cce35e051
wastenio da silva rocha <wastenio.silva@gmail.com>
2025-07-26 16:37:53 -0300

    Criação do projeto e as primeiras classes de entity;

---
+++ sop/src/main/java/com/sop/finance/SopApplication.java
@@ -0,0 +1,11 @@
+package com.sop.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SopApplication.class, args);
+    }
+}
